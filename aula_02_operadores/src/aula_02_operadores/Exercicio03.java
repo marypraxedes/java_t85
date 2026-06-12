a45896f27ad1ec08af033d971b5a00e4b6eeb68c
Maryane Praxedes <mary.praxedes@icloud.com>
2026-06-12 14:26:19 -0300

    Atividades práticas da aula 02

--- aula_02_operadores/src/aula_02_operadores/Exercicio03.java
+++ aula_02_operadores/src/aula_02_operadores/Exercicio03.java
@@ -8,23 +8,26 @@ public class Exercicio03 {
 		
 		Scanner leia = new Scanner(System.in);
 		
-		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+		float salarioBruto, adicionalNoturno, horasExtras, quantidadeExtra, descontos, salarioLiquido;
 		
 		System.out.println("--- SALÁRIO ---");
 		
 		System.out.printf("\nDigite o salário bruto: ");
 		salarioBruto = leia.nextFloat();
 		
-		System.out.printf("\nDigite o adicional noturno: ");
+		System.out.printf("\nDigite o valor do adicional noturno: ");
 		adicionalNoturno = leia.nextFloat();
 		
-		System.out.printf("\nDigite as horas extras: ");
+		System.out.printf("\nDigite o valor das horas extras: ");
 		horasExtras = leia.nextFloat();
+		
+		System.out.printf("\nDigite a quantidade de horas extras: ");
+		quantidadeExtra = leia.nextFloat();
 		
 		System.out.printf("\nDigite os descontos: ");
 		descontos = leia.nextFloat();
 		
-		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * quantidadeExtra) - descontos;
 				
 		System.out.printf("\nSalário Líquido: R$ %.2f ", salarioLiquido);
 		
